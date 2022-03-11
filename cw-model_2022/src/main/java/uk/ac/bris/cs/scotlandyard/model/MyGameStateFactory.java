dc34411b0897f2eb1fc629134345d0c65c57a5ea
Leo_Lai <leojacklai@outlook.com>
2022-03-11 20:25:36 +0000

    added return for GameState method in MyGameStateFactory.java

--- cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,25 +1,94 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
+import java.util.*;
 import javax.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
+
 
 /**
  * cw-model
  * Stage 1: Complete this class
  */
 public final class MyGameStateFactory implements Factory<GameState> {
+	
+	private final class MyGameState implements GameState{
 
-	@Nonnull @Override public GameState build(
-			GameSetup setup,
-			Player mrX,
-			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
+		private GameSetup setup; 			  // Thing to return
+		//-------------------------------------------------------
+		private Player mrX;		 			  // Holds Mr. x
+		private List<Player> detectives;      // Detectives
+		//-------------------------------------------------------
+		private ImmutableSet<Piece> remaining;// Pieces remaining
+		private ImmutableSet<Piece> winner;   // Current Winner(s)
+		//-------------------------------------------------------
+		private ImmutableList<LogEntry> log;  // Mr. x's move log
+		private ImmutableSet<Move> moves;	  // Holds the current possible moves
+		//--------------------------------------------------------------------------------------------------------------
 
+		private MyGameState(
+			final GameSetup setup,
+			final ImmutableSet<Piece> remaining,
+			final ImmutableList<LogEntry> log,
+			final Player mrX,
+			final List<Player> detectives) {
+
+		}
+
+		@Nonnull @Override //TODO
+		public GameSetup getSetup() {
+			return null;
+		}
+
+		@Nonnull @Override //TODO
+		public ImmutableSet<Piece> getPlayers() {
+			return null;
+		}
+
+		@Nonnull @Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return null;
+		}
+
+		@Nonnull @Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return null;
+		}
+
+		@Nonnull @Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return null;
+		}
+
+		@Nonnull @Override
+		public ImmutableSet<Piece> getWinner() {
+			return null;
+		}
+
+		@Nonnull @Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
+		@Nonnull @Override //from interface GameState //TODO
+		public GameState advance(Move move) {
+			return null;
+		}
 	}
+
+	@Nonnull @Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
+		// TODO
+		return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
+	}
+
+
+
 
 }
