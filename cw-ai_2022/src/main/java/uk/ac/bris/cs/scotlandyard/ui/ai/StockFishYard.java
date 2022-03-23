2090a88673d7d0b10903ed0981828306e3061bb0
Leo_Lai <leojacklai@outlook.com>
2022-03-23 12:53:40 +0000

    bfs first draft

--- cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/StockFishYard.java
+++ cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/StockFishYard.java
@@ -37,47 +37,42 @@ public class StockFishYard implements Ai {
 		}
 
 		int node;
-		
 		while(!queue.isEmpty()) {
-			node = queue.get(0);	//make the node we analyse the one at the front of the queue
-			queue.remove(0);	//we can now remove this node from the queue
+
+			node = queue.get(0);	// make the node we analyse the one at the front of the queue
+			queue.remove(0);	// we can now remove this node from the queue
 			for(Integer neighbour : board.getSetup().graph.adjacentNodes(node)) { // goes through all adjacent nodes to a
 				if (!visited.contains(neighbour)) {//if neighbour is a new unvisited node,
-					//add neighbour to the queue
-					queue.add(neighbour);
-					//make the neighbours a visited so they won't be visited again
-					visited.add(neighbour);
-					//make the previous nodes of each neighbour the current node
-					prev.set(neighbour, node);
+					queue.add(neighbour); // add neighbour to the queue
+					visited.add(neighbour); // make the neighbours a visited so they won't be visited again
+					prev.set(neighbour, node); // make the previous nodes of each neighbour the current node
 				}
 			}
 		}
 
-		// here we build the paths and calculate the path lengths for each
+		// here we build the paths and calculate the path lengths from each detective to mrX
 		int lengths = 0;
-		for (Integer location : getDetectiveLocations(board)) {
+
+		for (Integer location : getDetectiveLocations(board)) { //for each location, access its previous nodes all the way down
 			int length = 0;
 			int ptr = location;
-			while (ptr != source) {
+			while (ptr != source) { //until you reach the original source, increase length by one for each node and change pointer to the next previous node
 				length +=1;
 				ptr = prev.get(ptr);
 			}
-			lengths += length;
+			lengths += length; //add this length to the total lengths
 		}
 
-		return lengths;
+		return lengths; //return the total lengths
 	}
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
-//
-		int nodeTotal = board.getSetup().graph.nodes().size();
 
-		var moves = board.getAvailableMoves().asList();
+		var moves = board.getAvailableMoves().asList(); //list of all possible moves
 		List<Integer> scores = new ArrayList<>();
-		for (int i = 0; i < moves.size(); i++) {
+		for (int i = 0; i < moves.size(); i++) { //for each move, use visitor pattern to discern the final destination
 			Integer destination = moves.get(i).accept(new Move.Visitor<>() {
 				@Override
 				public Integer visit(Move.SingleMove move) {
@@ -89,9 +84,11 @@ public class StockFishYard implements Ai {
 					return move.destination2;
 				}
 			} );
-			int score = score(board, destination);
-			scores.add(score);
+			int score = score(board, destination); //use the destination and board with other detective location to calculate total score
+			scores.add(score); //add this to the array of scores
 		}
+
+		//below is an inefficient way to calculate the move with the highest score. should replace this.
 		int bestScore = 0;
 		int bestScoreIndex = 0;
 		for (int score : scores) {
@@ -104,11 +101,6 @@ public class StockFishYard implements Ai {
 		System.out.println(bestScore);
 
 		System.out.println(" ");
-
-		//Move move = moves.get(new Random().nextInt(moves.size()));
-
-//		System.out.println("scores" + score(board, move.source()));
-//		System.out.println("locations" + getDetectiveLocations(board));
 
 		return moves.get(bestScoreIndex);
 
