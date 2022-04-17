b931c1fca76ddde59b632fd0e198a7ed98aed4d1
Leo_Lai <leojacklai@outlook.com>
2022-04-17 19:24:00 +0100

    easter changes

--- cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/StockFishYard2.java
+++ cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/StockFishYard2.java
@@ -1,24 +1,23 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
 import java.util.*;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
-
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
 import io.atlassian.fugue.Pair;
-import uk.ac.bris.cs.scotlandyard.model.Ai;
-import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.Piece;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 public class StockFishYard2 implements Ai {
 
     @Nonnull @Override public String name() { return "StockFishYard2"; }
 
-    //------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
 
     private List<Integer> getDetectiveLocations(Board board) {  // gets the detective locations 
         List<Integer> locations = new ArrayList<>();
@@ -29,7 +28,7 @@ public class StockFishYard2 implements Ai {
         }
         return locations;
     }
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     @Nonnull private Integer biBFS_dist(@Nonnull Board board, Integer node1, Integer node2){
         //--------------------- for node1
         Map<Integer, Integer>  node1Map = new HashMap<Integer, Integer>(){{put(node1, null);}}; //contains previous node map
@@ -84,11 +83,125 @@ public class StockFishYard2 implements Ai {
         }
         return length;
     }
+
+    private double score(){
+        // calculates score
+        return 0.0;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    public ImmutableMap<ScotlandYard.Ticket, Integer> ticketMap(Board board, Piece piece){
+        List<ScotlandYard.Ticket> ticketTypes = new ArrayList<>(List.of(ScotlandYard.Ticket.DOUBLE, ScotlandYard.Ticket.BUS,ScotlandYard.Ticket.TAXI,ScotlandYard.Ticket.SECRET, ScotlandYard.Ticket.UNDERGROUND));
+        Map<ScotlandYard.Ticket, Integer> ticketMap = new HashMap<>();
+        for (ScotlandYard.Ticket ticket : ticketTypes) {
+            Optional<Board.TicketBoard> optTicket = board.getPlayerTickets(piece);
+            optTicket.ifPresent(tickValue -> ticketMap.put(ticket ,tickValue.getCount(ticket)));
+        }
+        return ImmutableMap.copyOf(ticketMap);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    public List<List<Move>> permutations (Board.GameState gameState, Boolean maximisingPlayer){
+        ImmutableSet<Move> allMoves = gameState.getAvailableMoves();
+        ImmutableSet<Piece> pieces = gameState.getPlayers();
+        Map<Piece, List<Move>> pieceMoves = new HashMap<Piece, List<Move>>();
+
+
+        if (!maximisingPlayer) {
+            for (Piece piece : pieces) {
+                if(piece.isDetective()){
+                    pieceMoves.put(piece, (allMoves.stream().filter(move -> move.commencedBy() == piece)).collect(Collectors.toList()));
+                }
+            }
+        }
+        else {
+            for (Piece piece : pieces) {
+                if(!piece.isDetective()){
+                    pieceMoves.put(piece, allMoves.stream().filter(move -> move.commencedBy() == piece).collect(Collectors.toList()));
+                }
+            }
+        }
+
+        if (!maximisingPlayer){
+            return Lists.cartesianProduct(pieceMoves.entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toList()));
+        }
+        else {
+            return List.of(allMoves.stream().filter(move -> !move.commencedBy().isDetective()).collect(Collectors.toList()));
+        }
+
+
+    }
+
+
+    public Integer miniMax (List<Move> moves, List<Player> detectives, Player mrX, int depth, int alpha, int beta, Boolean maximisingPlayer, MyGameStateFactory factory, Board board){
+        Board.GameState gamestate = null;
+            Player newX = null;
+            for (Move move : moves) {
+                if (move.commencedBy() == mrX.piece()) {
+                    newX = mrX.use(move.tickets()).at(move.accept(new DestinationVisitor()));
+                }
+                else {
+                    newX = mrX;
+                }
+            }
+        if (maximisingPlayer) {
+            gamestate = factory.build(board.getSetup(), newX, ImmutableList.copyOf(detectives));
+        }
+        else {
+            List<Player> newPlayers = new ArrayList<>();
+            for (Player detective : detectives) {
+                for (Move move : moves){
+                    if (move.commencedBy() == detective.piece()) {
+                        newPlayers.add(detective.use(move.tickets()).at(move.accept(new DestinationVisitor())));
+                    }
+                    else {
+                        newPlayers.add(detective);
+                    }
+                }
+            }
+            gamestate = factory.build(board.getSetup(), newX, ImmutableList.copyOf(newPlayers));
+
+        }
+
+     if (depth == 0 || !(gamestate.getWinner().isEmpty())) {
+         // return evaluation game score statically
+     };
+
+
+
+    }
+//    public List<List<>>
     //------------------------------------------------------------------------------------------------------------------
     @Nonnull @Override public Move pickMove(@Nonnull Board board, Pair<Long, TimeUnit> timeoutPair) {
         long start = System.nanoTime(); //for calculating how long a pickMove takes
-        //---------------
         var moves = board.getAvailableMoves().asList(); // mrx moves
+        //---------------
+
+        MyGameStateFactory factory = new MyGameStateFactory();
+
+        List<Player> detectives = new ArrayList<>();
+        Player mrX = null;
+
+        for (Piece piece : board.getPlayers()) {
+            if (piece.isDetective()) {
+                detectives.add(new Player(piece,
+                        ticketMap(board, piece),
+                        board.getDetectiveLocation((Piece.Detective) piece).orElse(0)));
+            }
+            else {
+                mrX = new Player(piece,
+                        ticketMap(board, piece),
+                        moves.get(0).source()
+                        );
+            }
+        }
+
+        Board.GameState first = factory.build(board.getSetup(), mrX, ImmutableList.copyOf(detectives));
+
+
+
+
         Map<Move, Integer> scores = new HashMap<>(); // map for scores for moves
 
         //a new visitor that returns the final destination of a move.
